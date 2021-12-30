1ab001620240961dbfb7a553d45ecbfaf00b3019
Savio Nascimento <savio.nascimento@beegol.com>
2021-12-30 11:02:55 -0300

    Funções extendidas

---
+++ src/main/kotlin/TesteFuncoesExtendidas.kt
@@ -0,0 +1,5 @@
+fun main() {
+    val salarios = arrayOf("4000".toBigDecimal(),"2900".toBigDecimal(),"3100".toBigDecimal())
+    println("Somatória: "+salarios.somatoria())
+    println("Média: "+salarios.media())
+}
