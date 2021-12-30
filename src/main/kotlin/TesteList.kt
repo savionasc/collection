028d7467d96e23b96955616c2de05f3a95da386c
Savio Nascimento <savio.nascimento@beegol.com>
2021-12-30 08:55:45 -0300

    Mais Operações em collection em List

---
+++ src/main/kotlin/TesteList.kt
@@ -0,0 +1,32 @@
+fun main() {
+    val savio = Funcionario("Sávio", 5000.0, "CLT")
+    val antonio = Funcionario("Antonio", 10000.0,"PJ")
+    val nascimento = Funcionario("Nascimento", 4500.0, "CLT")
+
+    val funcionarios = listOf<Funcionario>(savio, antonio, nascimento)
+
+    funcionarios.forEach{
+        println(it)
+    }
+
+    println("Funcionário encontrado:")
+    println(funcionarios.find { funcionario -> funcionario.nome == "Sávio" })
+    println("Ordenado por salário:")
+    funcionarios.sortedBy { it.salario }.forEach{ println(it) }
+    println("Ordenado por Nome:")
+    funcionarios.sortedBy { it.nome }.forEach{ println(it) }
+    println("Agrupado por Tipo de Contratação:")
+    funcionarios.groupBy { it.tipoContratacao }.forEach{ println(it) }
+}
+
+data class Funcionario(
+    val nome: String,
+    val salario: Double,
+    val tipoContratacao: String
+){
+    override fun toString(): String =
+        """
+            Nome:    $nome
+            Salário: $salario
+        """.trimIndent()
+}
