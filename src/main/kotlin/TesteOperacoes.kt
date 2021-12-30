e47ef0c2d0411e5247045ff1edb199f0f678089a
Savio Nascimento <savio.nascimento@beegol.com>
2021-12-30 08:26:36 -0300

    Mais Operações em collection

--- src/main/kotlin/TesteOperacoes.kt
+++ src/main/kotlin/TesteOperacoes.kt
@@ -12,7 +12,18 @@ fun main() {
 
     val salariosMaiorQue2500 = salarios.filter { it > 2500.0 }
 
-    println("-----------------")
+    println("-----------------\nSalários maiores que 2500:")
 
     salariosMaiorQue2500.forEach{ println(it) }
+
+    println("-----------------\nContando a quantidade de salários entre 2000 e 5000:")
+    println(salarios.count{ it in 2000.0..5000.0 })
+
+    println("-----------------\nBuscando elemento específico no array")
+    println(salarios.find { it == 4000.0 })
+    println(salarios.find { it == 2500.0 })
+
+    println("-----------------\nDescobrindo existência de elemento específico no array")
+    println(salarios.any { it == 4000.0 })
+    println(salarios.any { it == 2500.0 })
 }
